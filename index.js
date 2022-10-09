9f623a406112129f8e30552e9b34f84ddd98343c
Pogga1 <Andreweysoldt@gmail.com>
2022-10-09 14:37:14 -0700

    added necessary files to work the application

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+const HTMLGen = require('./source/teamGen');
+
+const Intern = require("./library/intern");
+const engineer = require("./library/engineer");
+const manager = require("./library/manager");
